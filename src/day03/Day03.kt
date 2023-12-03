f3f810a5779b68050e7cddcc9f7777285a6f7d61
Kristian Brudeli <krbrudeli@gmail.com>
2023-12-03 10:56:23 +0100

    Day 3 part 1 done

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -34,30 +34,36 @@ data class Number(
 fun main() {
     val NUMBER_PATTERN = """\d+""".toRegex()
     fun part1(input: List<String>): Int {
-//        val numRows = input.size
-//        val numCols = input.first().length
+        val numRows = input.size
+        val numCols = input.first().length
 
-//        val adjacentIndicesWithinBounds = { coord: Coordinate ->
-//            val rowIndices = (max(coord.row - 1, 0)..min(coord.row + 1, numRows - 1)).toList()
-//            val colIndices = (max(coord.col - 1, 0)..min(coord.col + 1, numCols - 1)).toList()
-//            rowIndices.map { rowIdx ->
-//                colIndices.map { colIdx ->
-//                    Coordinate(rowIdx, colIdx)
-//                }
-//            }.flatten()
-//        }
-        val matches = NUMBER_PATTERN.findAll(input.first())
-        println(matches)
+        val adjacentIndicesWithinBounds = { coord: Coordinate ->
+            val rowIndices = (max(coord.row - 1, 0)..min(coord.row + 1, numRows - 1)).toList()
+            val colIndices = (max(coord.col - 1, 0)..min(coord.col + 1, numCols - 1)).toList()
+            rowIndices.map { rowIdx ->
+                colIndices.map { colIdx ->
+                    Coordinate(rowIdx, colIdx)
+                }
+            }.flatten()
+        }
 
         val tiles = input.mapIndexed { rowIndex, line ->
             line.mapIndexed { colIndex, char ->
                 Tile(Coordinate(row = rowIndex, col = colIndex), value = char.toString())
             }
         }
-        val symbolTileMap = tiles.flatten().filter { it.isSymbol }.associateBy { it.coordinate }
-        println("$symbolTileMap")
+        val symbolTileSet = tiles.flatten().filter(Tile::isSymbol).map(Tile::coordinate).toSet()
+        val validNumbers = input.mapIndexed { rowIdx, line ->
+            val allNumberMatches = NUMBER_PATTERN.findAll(line).toList().map { it.groups }.flatten().filterNotNull()
+            allNumberMatches.filter { numberMatch ->
+                val numberCoords = numberMatch.range.map { colIdx -> Coordinate(rowIdx, colIdx) }
+                val adjacentCoords = numberCoords.map { adjacentIndicesWithinBounds(it) }.flatten()
+                val hasAdjacentSymbol = adjacentCoords.any { symbolTileSet.contains(it) }
+                hasAdjacentSymbol
+            }
+        }.flatten().map { it.value.toInt() }
 
-        return 0
+        return validNumbers.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -66,7 +72,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day03/Day03_test")
-//    check(part1(testInput) == 4361)
+    check(part1(testInput) == 4361)
 //    check(part2(testInput) == 2286)
 
     val input = readInput("day03/Day03")
