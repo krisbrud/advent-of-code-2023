26008d502641edc0cbcf1d9193f4bf6506403577
Kristian Brudeli <krbrudeli@gmail.com>
2023-12-03 10:33:42 +0100

    WIP day 3

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -2,27 +2,61 @@ package day03
 
 import println
 import readInput
+import kotlin.math.max
+import kotlin.math.min
 
 val NUMBERS = (0..9).map(Int::toString).toSet()
 val NON_SYMBOLS = NUMBERS + "."
+fun String.isDigit() = NUMBERS.contains(this)
 fun String.isSymbol() = !NON_SYMBOLS.contains(this)
 
+data class Coordinate(val row: Int, val col: Int)
+
 data class Tile(
-    val row: Int,
-    val col: Int,
+    val coordinate: Coordinate,
     val value: String,
 ) {
+    val isNumber = value.isDigit()
     val isSymbol = value.isSymbol()
 }
 
+data class Number(
+    val value: Int,
+    val firstCoord: Coordinate,
+) {
+    private val length = value.toString().length
+    val indices by lazy {
+        val colIndices = firstCoord.col until (firstCoord.col + length)
+        colIndices.map { Coordinate(firstCoord.row, it) }
+    }
+}
+
 fun main() {
+    val NUMBER_PATTERN = """\d+""".toRegex()
     fun part1(input: List<String>): Int {
+//        val numRows = input.size
+//        val numCols = input.first().length
+
+//        val adjacentIndicesWithinBounds = { coord: Coordinate ->
+//            val rowIndices = (max(coord.row - 1, 0)..min(coord.row + 1, numRows - 1)).toList()
+//            val colIndices = (max(coord.col - 1, 0)..min(coord.col + 1, numCols - 1)).toList()
+//            rowIndices.map { rowIdx ->
+//                colIndices.map { colIdx ->
+//                    Coordinate(rowIdx, colIdx)
+//                }
+//            }.flatten()
+//        }
+        val matches = NUMBER_PATTERN.findAll(input.first())
+        println(matches)
+
         val tiles = input.mapIndexed { rowIndex, line ->
             line.mapIndexed { colIndex, char ->
-                Tile(row=rowIndex, col=colIndex, value = char.toString())
+                Tile(Coordinate(row = rowIndex, col = colIndex), value = char.toString())
             }
         }
-        println("Tiles: $tiles")
+        val symbolTileMap = tiles.flatten().filter { it.isSymbol }.associateBy { it.coordinate }
+        println("$symbolTileMap")
+
         return 0
     }
 
