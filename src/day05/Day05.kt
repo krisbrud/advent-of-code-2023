e2d5b06cd02ac4630852c800712521e91cf6c663
Kristian Brudeli <krbrudeli@gmail.com>
2023-12-07 13:56:06 +0100

    Day 5 done

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -17,6 +17,10 @@ data class RangeMapping(
         val offset = sourceElement - source
         return destination + offset
     }
+
+    fun mapRange(inputRange: LongRange): LongRange? = sourceRange.intersection(inputRange)?.let { intersectionRange ->
+        mapOrThrow(intersectionRange.first)..mapOrThrow(intersectionRange.last)
+    }
 }
 
 data class RangeMappingGroup(
@@ -26,11 +30,37 @@ data class RangeMappingGroup(
         val containingMapping = mappings.firstOrNull { it.contains(element) }
         return containingMapping?.mapOrThrow(element) ?: element
     }
+
+//    fun mapRange(inputRange: LongRange): List<LongRange> {
+//        val sourceRangesWithMappings = mappings.filter { mapping ->
+//            inputRange.intersection(mapping.sourceRange) != null
+//        }.sortedBy { it.sourceRange.first }
+//        val feedThrough = when(sourceRangesWithMappings.size) {
+//            0 -> inputRange
+//            else -> {
+//                sourceRangesWithMappings.windowed(2).map { (prev, current) ->
+//                    val prevlast = prev.sourceRange.last
+//                    val currentFirst = current.sourceRange.first
+//                    if (prevlast < currentFirst) {
+//                        (prevlast)..(currentFirst)
+//                    } else null
+//                }.filterNotNull()
+//            }
+//        }
+//
+//
+//        val mappedRanges = sourceRangesWithMappings.mapNotNull { rangeMapping -> rangeMapping.mapRange(inputRange) }
+//        return mappedRanges + feedThrough
+//    }
 }
 
 fun List<RangeMappingGroup>.evaluate(element: Long): Long = fold(element) { acc, mappingGroup ->
     mappingGroup.mapElement(acc)
 }
+
+//fun List<RangeMappingGroup>.evaluate(seedRange: LongRange): List<LongRange> = fold(listOf(seedRange)) { inputRanges: List<LongRange>, mappingGroup: RangeMappingGroup ->
+//    inputRanges.map { inputRange -> mappingGroup.mapRange(inputRange) }.flatten()
+//}
 
 fun LongRange.intersection(other: LongRange): LongRange? {
     if (!(this.contains(other.first)
@@ -46,10 +76,10 @@ fun LongRange.intersection(other: LongRange): LongRange? {
 fun main() {
     val seedPattern = """seeds: (.+)""".toRegex()
     val mappingPattern = """(.+)-to-(.+) map:([\s\S]*?)\n\s*\n""".toRegex()
-    fun parseSeeds(input: String) = seedPattern.findAll(input).first().groups[1]!!.value.split(" ")
+    fun String.parseSeeds(): List<Long> = seedPattern.findAll(this).first().groups[1]!!.value.split(" ")
         .map(String::trim).map(String::toLong)
 
-    fun parseMappingGroups(input: List<String>): List<RangeMappingGroup> = mappingPattern.findAll((input + "\n\n").joinToString("\n"))
+    fun List<String>.parseMappingGroups(): List<RangeMappingGroup> = mappingPattern.findAll((this + "\n\n").joinToString("\n"))
         .map { it.groups }
         .toList()
         .map { matchGroupCollection ->
@@ -65,23 +95,9 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         // Find a way through the mappings
-        val inputWithNewlines = input + "\n\n"
-        val seeds = parseSeeds(inputWithNewlines.first())
+        val seeds = input.first().parseSeeds()
 
-        val mappingGroups = mappingPattern.findAll(inputWithNewlines.joinToString("\n"))
-            .map { it.groups }
-            .toList()
-            .map { matchGroupCollection ->
-                matchGroupCollection.toList().component4()!!.value.trim().split("\n")
-            }.map { lines ->
-                lines.map { line ->
-                    val (destination, source, length) = line.split(" ").map { it.toLong() }
-                    RangeMapping(destination = destination, source = source, length = length)
-                }
-            }.map { mappings ->
-                RangeMappingGroup(mappings)
-            }
-
+        val mappingGroups = input.parseMappingGroups()
 
         val locations = seeds.map { mappingGroups.evaluate(it) }
 
@@ -89,36 +105,22 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        val inputWithNewlines = input + "\n\n"
-        val seeds = parseSeeds(inputWithNewlines.first())
-        val seedRanges = seeds.chunked(2).map { (firstSeed, length) ->
+        val seeds = input.first().parseSeeds()
+        val seedRanges: List<LongRange> = seeds.chunked(2).map { (firstSeed, length) ->
             firstSeed..<(firstSeed + length)
         }
 
-        val mappingGroups = mappingPattern.findAll(inputWithNewlines.joinToString("\n"))
-            .map { it.groups }
-            .toList()
-            .map { matchGroupCollection ->
-                matchGroupCollection.toList().component4()!!.value.trim().split("\n")
-            }.map { lines ->
-                lines.map { line ->
-                    val (destination, source, length) = line.split(" ").map { it.toLong() }
-                    RangeMapping(destination = destination, source = source, length = length)
-                }
-            }.map { mappings ->
-                RangeMappingGroup(mappings)
-            }
+        val mappingGroups = input.parseMappingGroups()
 
+        val minLocation = seedRanges.map { seedRange -> seedRange.minOf { seed -> mappingGroups.evaluate(seed) } }.min()
 
-        val locations = seeds.map { mappingGroups.evaluate(it) }
-
-        return locations.min().toInt()
+        return minLocation.toInt()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day05/Day05_test")
     check(part1(testInput) == 35)
-//    check(part2(testInput) == 30)
+    check(part2(testInput) == 46)
 
     val input = readInput("day05/Day05")
     part1(input).println()
