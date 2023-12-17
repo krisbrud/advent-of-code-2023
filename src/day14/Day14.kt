34b07c9765ebf303cd4d24ffea4b3090f89f5117
Kristian Brudeli <krbrudeli@gmail.com>
2023-12-17 13:40:13 +0100

    WIP day 14 part 2

--- src/day14/Day14.kt
+++ src/day14/Day14.kt
@@ -2,6 +2,7 @@ package day14
 
 import println
 import readInput
+import kotlin.time.measureTime
 
 fun Char.isRoundedRock(): Boolean = this == 'O'
 fun Char.isSupportBeam(): Boolean = this == '#'
@@ -12,6 +13,9 @@ fun List<String>.transpose(): List<String> = first().indices.map { colIndex ->
     }.joinToString("")
 }
 
+fun List<String>.rotateRight(): List<String> = transpose().map(String::reversed)
+fun List<String>.rotateLeft(): List<String> = map(String::reversed).transpose()
+
 fun List<String>.tiltRocksNorth(): List<String> {
     // Pseudocode:
     // - transpose
@@ -19,8 +23,24 @@ fun List<String>.tiltRocksNorth(): List<String> {
     // - split into char arrays, sort descending, join back to strings (doesn't matter if we sort CharArrays of only support beams)
     // - join to string
     // - transpose back
-    val transposed = transpose()
-    val sorted = transposed.map { line ->
+    return transpose().map { line ->
+        line.fold(mutableListOf(mutableListOf<Char>())) { acc, c ->
+            if (c.isSupportBeam() || acc.last().all { it.isSupportBeam() }) {
+                acc.add(mutableListOf(c))
+            } else {
+                acc.last().add(c)
+            }
+            acc
+        }.map { list -> list.sortedDescending().joinToString("") }.joinToString("")
+    }.transpose()
+}
+
+fun List<String>.tiltRocksLeft(): List<String> {
+    // Pseudocode:
+    // - split lines into lists of strings containing only rocks and empty or support beams
+    // - split into char arrays, sort descending, join back to strings (doesn't matter if we sort CharArrays of only support beams)
+    // - join to string
+    return map { line ->
         line.fold(mutableListOf(mutableListOf<Char>())) { acc, c ->
             if (c.isSupportBeam() || acc.last().all { it.isSupportBeam() }) {
                 acc.add(mutableListOf(c))
@@ -30,8 +50,14 @@ fun List<String>.tiltRocksNorth(): List<String> {
             acc
         }.map { list -> list.sortedDescending().joinToString("") }.joinToString("")
     }
-    val result = sorted.transpose()
-    return result
+}
+
+fun List<String>.cycle(): List<String> {
+    return rotateLeft().tiltRocksLeft() // North
+        .rotateRight().tiltRocksLeft() // West
+        .rotateRight().tiltRocksLeft() // South
+        .rotateRight().tiltRocksLeft() // East
+        .rotateRight().rotateRight()
 }
 
 fun List<String>.calculateLoad(): Int = withIndex().sumOf { indexedLine ->
@@ -48,15 +74,33 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
+        val testIterations = 1000
+        val duration = measureTime {
+            var tmp = input
+            var i = 0
+
+            while (i < testIterations) {
+                tmp = tmp.cycle()
+                i += 1
+            }
+        }
+        println("Duration: $duration")
+        val durationPerCycle = duration / testIterations
+        println("Duration per cycle: $durationPerCycle")
+
+        val totalIterations = 1000000000
+        val estimatedTotalDuration = durationPerCycle * 1000000000
+        println("Estimated total duration: $estimatedTotalDuration")
+
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day14/Day14_test")
-    check(part1(testInput) == 136)
+//    check(part1(testInput) == 136)
 //    check(part2(testInput) == 400)
 
     val input = readInput("day14/Day14")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
--- src/day14/Day14Tests.kt
+++ src/day14/Day14Tests.kt
@@ -7,8 +7,8 @@ class Day14Tests {
     @Test
     fun `transpose should work as expected`() {
         assertEquals(listOf("123", "456"), listOf("14", "25", "36").transpose())
-        assertEquals(listOf("14", "25", "36"),listOf("123", "456").transpose())
-        assertEquals(listOf("13", "24"),listOf("12", "34").transpose())
+        assertEquals(listOf("14", "25", "36"), listOf("123", "456").transpose())
+        assertEquals(listOf("13", "24"), listOf("12", "34").transpose())
     }
 
     val beforeTilt = """
@@ -22,7 +22,7 @@ class Day14Tests {
     .......O..
     #....###..
     #OO..#....
-    """.trimIndent().split("\n")
+    """.trimIndent().splitByLines()
 
     val tilted = """
     OOOO.#.O..
@@ -35,7 +35,7 @@ class Day14Tests {
     ..O.......
     #....###..
     #....#....
-    """.trimIndent().split("\n")
+    """.trimIndent().splitByLines()
 
     @Test
     fun `tilting rocks north works as expected`() {
@@ -47,4 +47,68 @@ class Day14Tests {
         assertEquals(136, tilted.calculateLoad())
     }
 
+    fun String.splitByLines(): List<String> = split("\n")
+
+    val oneCycle = """
+    .....#....
+    ....#...O#
+    ...OO##...
+    .OO#......
+    .....OOO#.
+    .O#...O#.#
+    ....O#....
+    ......OOOO
+    #...O###..
+    #..OO#....
+    """.trimIndent().splitByLines()
+
+    val twoCycles = """
+    .....#....
+    ....#...O#
+    .....##...
+    ..O#......
+    .....OOO#.
+    .O#...O#.#
+    ....O#...O
+    .......OOO
+    #..OO###..
+    #.OOO#...O
+    """.trimIndent().splitByLines()
+
+    val threeCycles = """
+    .....#....
+    ....#...O#
+    .....##...
+    ..O#......
+    .....OOO#.
+    .O#...O#.#
+    ....O#...O
+    .......OOO
+    #...O###.O
+    #.OOO#...O
+    """.trimIndent().splitByLines()
+
+    @Test
+    fun `cycles should work as expected`() {
+        val actualOneCycle = beforeTilt.cycle()
+        val actualTwoCycles = actualOneCycle.cycle()
+        val actualThreeCycles = actualTwoCycles.cycle()
+        assertEquals(oneCycle, actualOneCycle)
+        assertEquals(twoCycles, actualTwoCycles)
+        assertEquals(threeCycles, actualThreeCycles)
+    }
+
+    @Test
+    fun `rotate right should work as expected`() {
+        val given = listOf("12", "34")
+        val expect = listOf("31", "42")
+        assertEquals(expect, given.rotateRight())
+    }
+
+    @Test
+    fun `rotate left should work as expected`() {
+        val given = listOf("31", "42")
+        val expect = listOf("12", "34")
+        assertEquals(expect, given.rotateLeft())
+    }
 }
